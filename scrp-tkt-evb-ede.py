13f5328c8cece2c470b9be71ba2a89ebb24087c2
FranUrru <146026713+FranUrru@users.noreply.github.com>
2026-02-05 12:02:03 -0300

    Update scrp-tkt-evb-ede.py

--- scrp-tkt-evb-ede.py
+++ scrp-tkt-evb-ede.py
@@ -486,6 +486,8 @@ def subir_a_google_sheets(df, nombre_tabla, nombre_hoja="sheet1", retries=3):
             
             # --- LÓGICA DE DETECCIÓN DE FILAS NUEVAS ---
             col_fecha = next((c for c in ['fecha de carga', 'Fecha Scrp'] if c in df_entrada.columns), None)
+            subset_duplicados = [c for c in columnas_posibles if c in df_entrada.columns]
+            columnas_posibles = ['Eventos', 'Nombre', 'title', 'Lugar', 'Locación', 'lugar', 'Origen', 'href', 'Fecha Convertida', 'Comienza']
             if len(existing_data) > 1:
                 existing_df = pd.DataFrame(existing_data[1:], columns=existing_data[0])
                 
@@ -1422,6 +1424,7 @@ print("Iniciando Ferias y Congresos...")
 ejecutar_scraper_ferias_y_congresos()
 
 enviar_log_smtp(contenido_final_log, destinatarios)
+
 
 
 
