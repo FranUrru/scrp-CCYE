80065f5363677fcc03f1d170a1fafa6a31c75530
FranUrru <146026713+FranUrru@users.noreply.github.com>
2026-06-12 14:12:38 -0300

    Update scrp-tkt-evb-ede.py

--- scrp-tkt-evb-ede.py
+++ scrp-tkt-evb-ede.py
@@ -1527,8 +1527,8 @@ def enviar_log_smtp(cuerpo_log, lista_destinatarios):
             log("🔴 Error: No se encontró EMAIL_APP_PASSWORD en los secretos.")
             return
         else:
-        # Esto te asegura que al menos tiene los 16 caracteres correctos y no espacios extra
-        log(f"🔑 Contraseña detectada. Largo: {len(password)} caracteres.")
+            # Esto te asegura que al menos tiene los 16 caracteres correctos y no espacios extra
+            log(f"🔑 Contraseña detectada. Largo: {len(password)} caracteres.")
           
 
         # Iniciamos la conexión con el servidor SMTP de Gmail
